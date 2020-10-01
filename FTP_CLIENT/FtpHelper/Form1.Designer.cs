4ae9c38afbb3286eb8a4a812272f8ea56d2f4752
TONY <37629628+tonyjosephsebastians@users.noreply.github.com>
2020-10-01 21:12:47 +0530

    Add files via upload

---
+++ FTP_CLIENT/FtpHelper/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FtpHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.UploadFile = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(83, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Host_Server";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 87);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(208, 20);
+            this.textBox1.TabIndex = 2;
+       
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(86, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(86, 226);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.PasswordChar = '*';
+            this.textBox3.Size = new System.Drawing.Size(208, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // UploadFile
+            // 
+            this.UploadFile.Location = new System.Drawing.Point(86, 298);
+            this.UploadFile.Name = "UploadFile";
+            this.UploadFile.Size = new System.Drawing.Size(208, 51);
+            this.UploadFile.TabIndex = 5;
+            this.UploadFile.Text = "Upload File";
+            this.UploadFile.UseVisualStyleBackColor = true;
+            this.UploadFile.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(83, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Username";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(83, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Password";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 387);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UploadFile);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "FTPClient";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button UploadFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FTP_CLIENT/FtpHelper/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace FtpHelper
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("FtpUploader.dll", CallingConvention = CallingConvention.Cdecl)]
+        //public static extern bool SetUpload([MarshalAs(UnmanagedType.LPWStr)]string FilePath, [MarshalAs(UnmanagedType.LPWStr)] string FileName, [MarshalAs(UnmanagedType.LPWStr)]string FTP_Server, [MarshalAs(UnmanagedType.LPWStr)]string Username, [MarshalAs(UnmanagedType.LPWStr)] string password);
+        public static extern bool Uploader([MarshalAs(UnmanagedType.LPWStr)]string FilePath, [MarshalAs(UnmanagedType.LPWStr)] string FileName, [MarshalAs(UnmanagedType.LPWStr)]string FTP_Server, [MarshalAs(UnmanagedType.LPWStr)]string Username, [MarshalAs(UnmanagedType.LPWStr)] string password);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+                
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fdlg = new OpenFileDialog();
+            fdlg.Title = "FTP_Client";
+            fdlg.InitialDirectory = @"c:\";
+            fdlg.Filter = "All files (*.*)|*.*|All files (*.*)|*.*";
+            fdlg.FilterIndex = 2;
+            fdlg.RestoreDirectory = true;
+            string Filename = "";
+            if (fdlg.ShowDialog() == DialogResult.OK)
+            {
+                Filename = Path.GetFileName(fdlg.FileName);
+                string Filepath = fdlg.FileName;
+                string fileserver = textBox1.Text;
+                string username = textBox2.Text;
+                string password = textBox3.Text;
+                // bool upload = SetUpload(Filename, Filename, textBox1.Text.tost, textBox2.Text, textBox3.Text);
+                bool upload = Uploader(Filename, Filepath, fileserver, username, password);
+
+                if (upload == false)
+                {
+                    string message = "Upload Failed";
+                    string title = "FTP_CLIENT";
+                    MessageBox.Show(message, title);
+                }
+                else
+                {
+                    string message = "Upload Success";
+                    string title = "FTP_CLIENT";
+                    MessageBox.Show(message, title);
+                }
+            }
+        }
+
+    }
+}
